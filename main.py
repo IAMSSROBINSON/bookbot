c32add46ad70ed2dc99610ad8c7c112299ca83c0
SSR <iamssrobinson@gmail.com>
2024-05-27 20:28:50 +0100

    Single responsibility functions.

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 def main():
   file_name = "books/frankenstein.txt"
 
-  with open(file_name) as f:
-      file_content = f.read()
-    
-  words_in_document = word_count(file_content)
-  letters_count_in_document = count_letters(file_content)
-  sorted_letters = sort_for_report(letters_count_in_document)
-  print_report(sorted_letters, file_name, words_in_document)
+  file_content = get_file_content(file_name)
+  words_in_doc = word_count(file_content)
+  letters_count_dict = count_letters(file_content)
+  sorted_letters = sort_for_report(letters_count_dict)
+  print_report(sorted_letters, file_name, words_in_doc)
 
 def print_report(sorted_list, file_name, words_in_doc_count):
   report_template_start = f"--- Begin report of {file_name} ---"
@@ -40,26 +38,30 @@ def sort_for_report(letters_dict):
   list.sort(reverse=True, key=sort_on)
   return list
   
-def count_letters(content_string):
+def count_letters(file_content):
+  words_list = file_content.split()
   letters = {}
-  words = content_string.split()
   
-  for word in words:
+  for word in words_list:
     for char in word:
       char_lower = char.lower()
       letters[char_lower] = 0
     
-  for word in words:
+  for word in words_list:
     for char in word:
       char_lower = char.lower()
       letters[char_lower] += 1
   return letters
 
 def word_count(file_content):
-  words_split = file_content.split()
-  words = len(words_split)
-  words_in_doc = len(words_split)
-  return words_in_doc
+  word_list = file_content.split()
+  words_count = len(word_list)
+  return words_count
+
+def get_file_content(file_name):
+   with open(file_name) as f:
+      file_content = f.read()
+      return file_content
   
 main()
 
